08c5d5afa061637def5536f183adbf3475745696
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-11-21 18:28:07 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,11 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  def show
+    # finding a cheese using id  from the url
+    # return json of the cheese
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+
+  # dynamic routes
+  get '/cheeses/:id', to: 'cheeses#show'
+
+
 end
